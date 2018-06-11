86f81e1834d9651c24b4104c1b013e4b8f2eb5c6
Teo Si-Yan <teosiyan@hotmail.com>
2018-06-11 15:23:17 +0800

    Add shuffle to dataloader

--- ants_bees.py
+++ ants_bees.py
@@ -36,10 +36,8 @@ img_datasets = {
 
 dataset_sizes = {x: len(img_datasets[x]) for x in ['train', 'val']}
 
-dataloaders = {
-  'train': torch.utils.data.DataLoader(img_datasets['train'], batch_size = 8),
-  'val': torch.utils.data.DataLoader(img_datasets['val'], batch_size = 16)
-}
+dataloaders = {x: torch.utils.data.DataLoader(img_datasets[x], batch_size=4, shuffle=True, num_workers=4)
+    for x in ['train', 'val']}
 
 class_names = img_datasets['train'].classes
 
